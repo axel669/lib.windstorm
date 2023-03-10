cd6be90e81c0119598b2baf960a9530477eed7cb
Axel669 <haryuu.hanekata@gmail.com>
2023-03-09 19:49:07 -0800

    commit thing that works so im not sad if something bad happens

---
+++ parse.mjs
@@ -0,0 +1,33 @@
+const funcs = /(?<key>(?<mod>\w+:)?(?<name>[\w\-]+)\[(?<args>.*?)\])/g
+const factor = { "(": 1, ")": -1 }
+const parseWind = (name) =>
+    // [...name.replace(/(,| )\s+/g, "$1").matchAll(funcs)]
+    [...name.matchAll(funcs)]
+    .map(
+        (match) => {
+            const { groups } = match
+            const argSource = groups.args
+            const s = argSource.split("").reduce(
+                (s, c) => {
+                    if (c === "," && s.p === 0) {
+                        s.args.push(s.c.trim())
+                        s.c = ""
+                        return s
+                    }
+                    s.p += factor[c] ?? 0
+                    s.c += c
+                    return s
+                },
+                { args: [], c: "", p: 0 }
+            )
+            const args = [...s.args, s.c.trim()]
+            return {
+                name: groups.name,
+                mod: groups.mod,
+                args,
+                key: match[0]
+            }
+        }
+    )
+
+export default parseWind
