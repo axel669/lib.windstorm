3bccb9e6db59ae269c402caaea33696b425cf41d
Axel669 <haryuu.hanekata@gmail.com>
2023-03-16 21:37:37 -0700

    updated tests for new components

--- test/comp/label.mjs
+++ test/comp/label.mjs
@@ -1,12 +1,19 @@
 export default () => {
     return html`
-        <label wind-storm="toggle[left] pad[4px] border[1px solid white] round[4px]">
-            <div wind-storm="label-content">ok but what if</div>
-            <input type="checkbox" />
-        </label>
-        <label wind-storm="toggle[right] color[primary] hover:bg[#dddddd60]">
-            <div wind-storm="label-content">ok but what if</div>
-            <input type="checkbox" />
-        </label>
+        <div wind-storm="flex[column"] width[360px]">
+            <label wind-storm="toggle[left] pad[4px] border[1px solid white] round[4px]">
+                <div wind-storm="label-content">ok but what if</div>
+                <input type="checkbox" />
+            </label>
+            <label wind-storm="toggle[right] color[primary] hover:bg[#dddddd60]">
+                <div wind-storm="label-content">ok but what if</div>
+                <input type="checkbox" />
+            </label>
+
+            <label wind-storm>
+                <div wind-storm="label-content">Cool Input</div>
+                <input type="text" />
+            </label>
+        </div>
     `
 }
