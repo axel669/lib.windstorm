0dac0ece546374815774081e9a649847c3c635ba
Axel669 <haryuu.hanekata@gmail.com>
2023-03-24 15:53:34 -0700

    updated tests

--- test/comp/button.mjs
+++ test/comp/button.mjs
@@ -3,25 +3,25 @@ export default () => {
     const inc = () => setCount(count() + 1)
     return html`
         <div
-        w$="grid gr-col[repeat(3, 1fr)] p[4px] gap[4px] w[480px]"
+        ws-x="grid gr-col[repeat(3,1fr)] p[4px] gap[4px] w[480px]"
         onClick=${inc}>
-            <button w$="@flat $color[primary] r[12px]">Primary</button>
-            <button w$="@outline $color[primary] r[12px]">Primary</button>
-            <button w$="@fill $color[primary]">Primary</button>
+            <button ws-x="@flat $color[primary] r[12px]">Primary</button>
+            <button ws-x="@outline $color[primary] r[12px]">Primary</button>
+            <button ws-x="@fill $color[primary]">Primary</button>
 
-            <button w$="@flat $color[secondary]">Secondary</button>
-            <button w$="@outline $color[secondary]">Secondary</button>
-            <button w$="@fill $color[secondary]">Secondary</button>
+            <button ws-x="@flat $color[secondary]">Secondary</button>
+            <button ws-x="@outline $color[secondary]">Secondary</button>
+            <button ws-x="@fill $color[secondary]">Secondary</button>
 
-            <button w$="@flat $color[danger]">Danger</button>
-            <button w$="@outline $color[danger]">Danger</button>
-            <button w$="@fill $color[danger]">Danger</button>
+            <button ws-x="@flat $color[danger]">Danger</button>
+            <button ws-x="@outline $color[danger]">Danger</button>
+            <button ws-x="@fill $color[danger]">Danger</button>
 
-            <button w$="@flat $color[warning]">Warning</button>
-            <button w$="@outline $color[warning]">Warning</button>
-            <button w$="@fill $color[warning]">Warning</button>
+            <button ws-x="@flat $color[warning]">Warning</button>
+            <button ws-x="@outline $color[warning]">Warning</button>
+            <button ws-x="@fill $color[warning]">Warning</button>
 
-            <div w$="col[span 3] bd[1px solid] p[4px]">
+            <div ws-x="col[span 3] bd[1px solid] p[4px]">
                 Click Count: ${count}
             </div>
         </div>
