1083374e445eae1d39dc741a33e678f96a309403
Axel669 <haryuu.hanekata@gmail.com>
2023-03-24 00:08:26 -0700

    > node processing improvements
    - attaching styles is now done by cloning a node and inserting before the main sheet so that func always overwrite component styles
    - component checking simplified so that it can be done faster

--- src/process-node.mjs
+++ src/process-node.mjs
@@ -2,23 +2,34 @@ import parseWind from "./parse.mjs"
 import windFuncs from "./wind-funcs.mjs"
 import { head, style } from "./baseline.mjs"
 
+console.log("wat", import.meta.url)
+
 const sheet = style.sheet
 const styles = {}
+const checkComponentFunc = (info) => {
+    if (info.name.startsWith("@") === false) {
+        return
+    }
+    const tag = info.node.tagName.toLowerCase()
+    if (tag === "label" && (info.name === "@ctrl" || info.name === "@t-ctrl")) {
+        attachStyle(`comp/${info.name.slice(1)}`)
+        return
+    }
+    attachStyle("comp/button")
+}
 const checkClass = (info) => {
-    const process = (
-        info !== null
-        && styles[info.key] === undefined
-        && windFuncs[info.name] !== undefined
-    )
-    if (process === false) {
+    if (info === null) {
+        return
+    }
+    checkComponentFunc(info)
+    if (styles[info.key] !== undefined || windFuncs[info.name] === undefined) {
         return
     }
     const func = windFuncs[info.name]
     const mod = (info.mod !== undefined) ? `:${info.mod.slice(0, -1)}` : ""
     const rules = func(...info.args)
-    const ruleText = Array.isArray(rules) ? rules.join(";\n") : rules
     sheet.insertRule(
-        `[wind-storm][wind-storm*="${info.key}"]${mod} {\n${ruleText};\n}`,
+        `[w\\$][w\\$*="${info.key}"]${mod} {\n${rules.join(";\n")};\n}`,
         sheet.cssRules.length
     )
     styles[info.key] = sheet.cssRules[sheet.cssRules.length - 1]
@@ -31,47 +42,37 @@ const attachStyle = (name) => {
         return
     }
 
-    const style = link.cloneNode()
-    style.href = `${name}.css`
-    style.rel = "stylesheet"
-    componentStyle[name] = style
-    head.appendChild(style)
+    const newStyle = link.cloneNode()
+    newStyle.href = `${name}.css`
+    newStyle.rel = "stylesheet"
+    componentStyle[name] = newStyle
+    head.insertBefore(newStyle, style)
     console.log(componentStyle)
 }
 const checkTheme = (node) => {
-    const theme = node.getAttribute("wind-theme")
+    const theme = node.getAttribute("w$-theme")
     if (theme === null) {
         return
     }
 
     attachStyle(`theme/${theme}`)
 }
-const inlabel = (node) => (
-    node.parentNode.tagName?.toLowerCase() === "label"
-    && node.parentNode.hasAttribute("wind-storm") === true
-)
-const standard = (node) => node.hasAttribute("wind-storm")
-const components = {
-    button: standard,
-    select: inlabel,
-    input: inlabel,
-}
 const checkComponent = (node) => {
     const tag = node.tagName.toLowerCase()
-    if (components[tag]?.(node) !== true) {
+    if (tag.startsWith("w$-") === false) {
         return
     }
 
-    attachStyle(`comp/${tag}`)
+    attachStyle(`comp/${tag.slice(3)}`)
 }
 const processNode = (node) => {
     checkTheme(node)
     checkComponent(node)
-    const storm = node.getAttribute("wind-storm")
+    const storm = node.getAttribute("w$")
     if (storm === null) {
         return
     }
-    const stuff = parseWind(storm)
+    const stuff = parseWind(storm, node)
     stuff.forEach(checkClass)
 }
 
