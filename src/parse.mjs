65e066321bff1e3d89068ce5ce2b1490d61af965
Axel669 <haryuu.hanekata@gmail.com>
2023-03-24 00:05:28 -0700

    parser uses indexes instead of concatenating for small speed gain

--- src/parse.mjs
+++ src/parse.mjs
@@ -1,30 +1,40 @@
-const funcs = /(?<key>(?<mod>\w+:)?(?<name>[\w\-]+)\[(?<args>.*?)\])/g
+const funcs = /(?<key>(?<mod>\w+:)?(?<name>[\$\@\w\-]+)(\[(?<args>.*?)\])?)/g
 const factor = { "(": 1, ")": -1 }
-const parseWind = (name) => {
+const parseArgs = (argSource) => {
+    const source = argSource.trim()
+    if (source === "") {
+        return []
+    }
+    const s = source.split("").reduce(
+        (s, c, i) => {
+            if (c === "," && s.p === 0) {
+                s.args.push(
+                    source.substring(s.s, i).trim()
+                )
+                s.s = i
+                return s
+            }
+            s.p += factor[c] ?? 0
+            return s
+        },
+        { args: [], p: 0, s: 0 }
+    )
+    return [
+        ...s.args,
+        source.slice(s.s).trim()
+    ]
+}
+const parseWind = (name, node) => {
     const matches = [...name.matchAll(funcs)]
     return matches.map(
         (match) => {
             const { groups } = match
-            const argSource = groups.args
-            const s = argSource.split("").reduce(
-                (s, c) => {
-                    if (c === "," && s.p === 0) {
-                        s.args.push(s.c.trim())
-                        s.c = ""
-                        return s
-                    }
-                    s.p += factor[c] ?? 0
-                    s.c += c
-                    return s
-                },
-                { args: [], c: "", p: 0 }
-            )
-            const args = [...s.args, s.c.trim()]
             return {
                 name: groups.name,
                 mod: groups.mod,
-                args,
-                key: match[0]
+                args: parseArgs(groups.args ?? ""),
+                key: match[0],
+                node,
             }
         }
     )
