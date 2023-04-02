dcc4172d1aca639dbe870894c08ed0cf87a0d511
Axel669 <haryuu.hanekata@gmail.com>
2023-04-01 21:11:53 -0700

    alpha-ready tests/previews

--- test/demo.mjs
+++ test/demo.mjs
@@ -34,53 +34,10 @@ const App = () => {
                 </ws-text>
             </ws-titlebar>
             <ws-paper ws-x="$content">
-                <ws-tabs onClick=${changeTheme} ws-x="$header">
+                <ws-tabs onClick=${changeTheme} ws-x="@solid $header">
                     ${tabs}
                 </ws-tabs>
-                <ws-flex ws-x="$content over-x[auto]" innerHTML=${example}>
-                </ws-flex>
-            </ws-paper>
-
-            <ws-paper ws-x="hide">
-                <ws-flex ws-x="over[auto] $content">
-                    <label for="modal" ws-x="@button @outline $color[accent]">
-                        Show Modal
-                    </label>
-                    <input type="checkbox" id="modal" ws-x="hide" />
-                    <ws-modal>
-                        <ws-paper ws-x="@select">
-                            <ws-flex ws-x="$content w[25vw]">
-                                <label for="modal" ws-x="@button @fill $color[accent]">
-                                    Close
-                                </label>
-                                <ws-tabs ws-x="@solid $vert" onClick=${changeTheme}>
-                                    ${tabs}
-                                </ws-tabs>
-                                <div ws-x="t-ws[pre]">
-                                    a div with lots of text that might push the width?
-                                </div>
-                                <ws-popover ws-x="">
-                                    <label ws-x="@button @fill $color[warning]" onClick=${() => setShow(true)}>
-                                        Blep
-                                    </label>
-                                    <ws-paper ws-x="$content inset-x[0px] y[50%]">
-                                        <div ws-x="$content">
-                                            <label ws-x="@button @fill $color[warning]" onClick=${() => setShow(false)}>
-                                                Close
-                                            </label>
-                                        </div>
-                                    </ws-paper>
-                                </ws-popover>
-                            </ws-flex>
-                        </ws-paper>
-                    </ws-modal>
-                    <ws-paper ws-theme="tron" ws-x="@outline">
-                        <ws-titlebar ws-x="$header">
-                            <ws-text ws-x="$title $title-text">
-                                Tron Theme
-                            </ws-text>
-                        </ws-titlebar>
-                    </ws-paper>
+                <ws-flex ws-x="$content over[auto]" innerHTML=${example}>
                 </ws-flex>
             </ws-paper>
         </ws-screen>
