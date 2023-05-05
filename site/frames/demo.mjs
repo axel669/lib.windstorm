07c04a7aa811870037bf366ec4c22856efef17ea
Axel669 <haryuu.hanekata@gmail.com>
2023-05-04 22:04:09 -0700

    version inc, refresh docs

--- site/frames/demo.mjs
+++ site/frames/demo.mjs
@@ -14,18 +14,19 @@ const example =
     ? await loadHTML(`./comp/${initial}.html`)
     : Array.from(
         { length: 100 },
-        () => `<div ws-x="&test[teal] b-r[1px_solid_var(--test)]">${Math.random()}</div>`
+        () => `<span ws-x="block &test[teal] b-r[1px_solid_var(--test)]">${Math.random()}</span>`
     ).join("\n")
 
 const name = `${initial.slice(0, 1).toUpperCase()}${initial.slice(1)}`
 
+const { wsx } = windstorm
 const App = () => {
     const [theme, set] = useState("dark")
 
     const changeTheme = (evt) => set(evt.target.dataset.theme)
     const tabs = ["light", "dark", "tron"].map(
         (th) => html`
-            <ws-tab tab-selected=${th === theme} data-theme=${th}>
+            <ws-tab ws-x="${wsx({ "$tab-selected": th === theme })}" data-theme=${th}>
                 ${th.slice(0, 1).toUpperCase()}${th.slice(1)}
             </ws-tab>
         `
