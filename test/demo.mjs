3754ef22ccda08fd9c24e9d7686e2b84710b0667
axel669 <haryuu.hanekata@gmail.com>
2023-11-13 23:43:22 -0800

    going to need to figure out if i still need the tests folder soon

--- test/demo.mjs
+++ test/demo.mjs
@@ -14,44 +14,36 @@ const example =
     ? await loadHTML(`./comp/${initial}.html`)
     : Array.from(
         { length: 100 },
-        () => `<span ws-x="block &test[teal] b-r[1px_solid_var(--test)]">${Math.random()}</span>`
+        () => `<span ws-x="block &test[teal] b-r[1px solid &test]">${Math.random()}</span>`
     ).join("\n")
 
 const name = `${initial.slice(0, 1).toUpperCase()}${initial.slice(1)}`
 
-const { wsx, custom, prop } = windstorm
-
-custom(
-    "blep",
-    (_, color) => [
-        prop("background", `linear-gradient(teal, ${color})`)
-    ]
-)
 const App = () => {
     const [theme, set] = useState("dark")
 
     const changeTheme = (evt) => set(evt.target.dataset.theme)
     const tabs = ["light", "dark", "tron"].map(
         (th) => html`
-            <ws-tab ws-x="${wsx({ "$tab-selected": th === theme })}" data-theme=${th}>
+            <ws-tab ws-x=${ws.x({ "$tab-selected": th === theme})} data-theme=${th}>
                 ${th.slice(0, 1).toUpperCase()}${th.slice(1)}
             </ws-tab>
         `
     )
 
     return html`
-        <ws-screen ws-x="@left theme[${theme}]">
+        <ws-screen ws-x="$theme[${theme}]">
             <ws-paper ws-x="r[0]">
-                <ws-titlebar ws-x="@fill $color[primary]" slot="header">
-                    <ws-icon class="ti-clipboard" ws-x="slot[title] $title">
+                <ws-titlebar ws-x="$fill $color[primary]" slot="header">
+                    <ws-icon class="ti-clipboard" ws-x="$title" slot="title">
                         ${name} Examples
                     </ws-icon>
                 </ws-titlebar>
-                <ws-paper ws-x="slot[content] r[0]">
-                    <ws-tabs onClick=${changeTheme} ws-x="@solid slot[header] p[0]">
+                <ws-paper ws-x="r[0]" slot="content">
+                    <ws-tabs onClick=${changeTheme} ws-x="$solid p[0]" slot="header">
                         ${tabs}
                     </ws-tabs>
-                    <ws-flex ws-x="slot[content] over[auto]" innerHTML=${example}>
+                    <ws-flex slot="content" ws-x="over[auto]" innerHTML=${example}>
                     </ws-flex>
                 </ws-paper>
             </ws-paper>
