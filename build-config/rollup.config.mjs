50b5a5f6060f834932259d81ef4e6ddb7a3a5045
axel669 <haryuu.hanekata@gmail.com>
2025-08-25 22:50:20 -0700

    [remove] no need to import yaml for macros, reduce build target count

--- build-config/rollup.config.mjs
+++ build-config/rollup.config.mjs
@@ -4,7 +4,6 @@ import { minify } from "csso"
 import fs from "fs-jetpack"
 import del from "rollup-plugin-delete"
 import sassc from "sass"
-// import yaml from "js-yaml"
 import terser from "@rollup/plugin-terser"
 
 const packageInfo = fs.read("package.json", "json")
@@ -40,30 +39,6 @@ const componentList = {
         return `export default ${JSON.stringify(styles)}`
     },
 }
-// const simpleFuncs = {
-//     load(id) {
-//         if (id.endsWith(".yml") === false) {
-//             return
-//         }
-
-//         const simple = yaml.load(
-//             fs.read(id, "utf8")
-//         )
-//         const lines =
-//             Object.entries(simple.funcs)
-//             .map(
-//                 ([name, prop]) => {
-//                     const key = name.replace(/\./g, "\\\\.")
-//                     if (Array.isArray(prop) === true) {
-//                         const parts = prop.map(prop => `"${prop}: {$}"`)
-//                         return `--${key}: ${parts.join(" ")};`
-//                     }
-//                     return `--${key}: "${prop}: {$}";`
-//                 }
-//             )
-//         return `export default \`${lines.join("")}\``
-//     }
-// }
 const libVersion = {
     resolveId(id) {
         if (id !== "$package") {
@@ -87,38 +62,47 @@ export default {
     input: "lib/main.mjs",
     output: [
         {
-            file: "dist/browser.js",
-            format: "iife",
-            name: "ws"
-        },
-        {
-            file: `test/preview/browser.js`,
-            format: "iife",
-            name: "ws"
-        },
-        {
-            file: ".ipsen/static/windstorm.js",
-            format: "iife",
-            name: "ws"
-        },
-        {
-            file: "dist/module.js",
+            file: "dist/windstorm.js",
             format: "esm",
         },
         {
-            file: "test/preview/module.js",
-            format: "esm",
+            file: "dist/ws.js",
+            format: "iife",
+            name: "ws",
         },
+        // {
+        //     file: "dist/browser.js",
+        //     format: "iife",
+        //     name: "ws"
+        // },
+        // {
+        //     file: `test/preview/browser.js`,
+        //     format: "iife",
+        //     name: "ws"
+        // },
+        // {
+        //     file: ".ipsen/static/windstorm.js",
+        //     format: "iife",
+        //     name: "ws"
+        // },
+        // {
+        //     file: "dist/module.js",
+        //     format: "esm",
+        // },
+        // {
+        //     file: "test/preview/module.js",
+        //     format: "esm",
+        // },
         {
             file: "test/debug/ws.js",
-            format: "esm",
-            sourcemap: true
+            format: "iife",
+            name: "ws",
+            sourcemap: true,
         },
     ],
     plugins: [
         del({ targets: "dist/*" }),
         componentList,
-        // simpleFuncs,
         libVersion,
         terser(),
     ]
