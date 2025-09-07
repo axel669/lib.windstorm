fd9a6b8ac3cb588c7de69984eeeda45e17977198
axel669 <haryuu.hanekata@gmail.com>
2025-09-07 02:31:43 -0700

    [add] split new version into smaller files, nc version created
    nc version has no components defined, just the macro system on its own, and the built in macros

--- build-config/rollup.config.mjs
+++ build-config/rollup.config.mjs
@@ -1,9 +1,9 @@
-import path from "node:path"
+// import path from "node:path"
 
-import { minify } from "csso"
+// import { minify } from "csso"
 import fs from "fs-jetpack"
 import del from "rollup-plugin-delete"
-import sassc from "sass"
+// import sassc from "sass"
 import terser from "@rollup/plugin-terser"
 
 const packageInfo = fs.read("package.json", "json")
@@ -12,33 +12,33 @@ fs.write(
     "readme.md",
     fs.read("docs/index.md").replaceAll("$VERSION", packageInfo.version)
 )
-const componentList = {
-    resolveId(id) {
-        if (id !== "$$component-css") {
-            return
-        }
-        return id
-    },
-    load(id) {
-        if (id !== "$$component-css") {
-            return
-        }
+// const componentList = {
+//     resolveId(id) {
+//         if (id !== "$$component-css") {
+//             return
+//         }
+//         return id
+//     },
+//     load(id) {
+//         if (id !== "$$component-css") {
+//             return
+//         }
 
-        const files = fs.find(
-            "lib",
-            { matching: ["**/*.sass", "!**/$*"] }
-        )
-        const styles = []
-        for (const file of files) {
-            styles.push({
-                name: path.basename(file, path.extname(file)),
-                style: minify(sassc.compile(file).css).css,
-            })
-        }
+//         const files = fs.find(
+//             "lib",
+//             { matching: ["**/*.sass", "!**/$*"] }
+//         )
+//         const styles = []
+//         for (const file of files) {
+//             styles.push({
+//                 name: path.basename(file, path.extname(file)),
+//                 style: minify(sassc.compile(file).css).css,
+//             })
+//         }
 
-        return `export default ${JSON.stringify(styles)}`
-    },
-}
+//         return `export default ${JSON.stringify(styles)}`
+//     },
+// }
 const libVersion = {
     resolveId(id) {
         if (id !== "$package") {
@@ -58,29 +58,48 @@ const libVersion = {
     }
 }
 
-export default {
-    input: "lib/main.mjs",
-    output: [
-        {
-            file: "dist/ws-esm.js",
-            format: "esm",
-        },
-        {
-            file: "dist/ws-standalone.js",
-            format: "iife",
-            name: "ws",
-        },
-        {
-            file: "docs/windstorm.js",
-            format: "iife",
-            name: "ws",
-            sourcemap: true,
-        },
-    ],
-    plugins: [
-        del({ targets: "dist/*" }),
-        componentList,
-        libVersion,
-        terser(),
-    ]
-}
+export default [
+    {
+        input: "lib/nc.js",
+        output: [
+            {
+                file: "dist/ws-nc-esm.js",
+                format: "esm",
+            },
+            {
+                file: "dist/ws-nc-es5.js",
+                format: "iife",
+                name: "ws",
+            }
+        ],
+        plugins: [
+            del({ targets: "dist/*" }),
+            libVersion,
+            terser(),
+        ]
+    },
+    {
+        input: "lib/full.js",
+        output: [
+            {
+                file: "dist/ws-esm.js",
+                format: "esm",
+            },
+            {
+                file: "dist/ws-es5.js",
+                format: "iife",
+                name: "ws",
+            },
+            {
+                file: "docs/ws-es5.js",
+                format: "iife",
+                name: "ws",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            libVersion,
+            terser(),
+        ]
+    },
+]
