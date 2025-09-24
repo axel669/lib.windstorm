c2340fe51a236d3a0f4ae47d63ff2f8aa04beb1b
axel669 <haryuu.hanekata@gmail.com>
2025-09-23 23:22:56 -0700

    [remove] old css importing mechanisms

--- build-config/rollup.config.mjs
+++ build-config/rollup.config.mjs
@@ -1,10 +1,7 @@
-// import path from "node:path"
-
-// import { minify } from "csso"
 import fs from "fs-jetpack"
 import del from "rollup-plugin-delete"
-// import sassc from "sass"
 import terser from "@rollup/plugin-terser"
+import resolve from "@rollup/plugin-node-resolve"
 
 const packageInfo = fs.read("package.json", "json")
 
@@ -12,33 +9,7 @@ fs.write(
     "readme.md",
     fs.read("docs/index.md").replaceAll("$VERSION", packageInfo.version)
 )
-// const componentList = {
-//     resolveId(id) {
-//         if (id !== "$$component-css") {
-//             return
-//         }
-//         return id
-//     },
-//     load(id) {
-//         if (id !== "$$component-css") {
-//             return
-//         }
 
-//         const files = fs.find(
-//             "lib",
-//             { matching: ["**/*.sass", "!**/$*"] }
-//         )
-//         const styles = []
-//         for (const file of files) {
-//             styles.push({
-//                 name: path.basename(file, path.extname(file)),
-//                 style: minify(sassc.compile(file).css).css,
-//             })
-//         }
-
-//         return `export default ${JSON.stringify(styles)}`
-//     },
-// }
 const libVersion = {
     resolveId(id) {
         if (id !== "$package") {
@@ -74,6 +45,7 @@ export default [
         ],
         plugins: [
             del({ targets: "dist/*" }),
+            resolve(),
             libVersion,
             terser(),
         ]
@@ -90,16 +62,25 @@ export default [
                 format: "iife",
                 name: "ws",
             },
+        ],
+        plugins: [
+            resolve(),
+            libVersion,
+            terser(),
+        ]
+    },
+    {
+        input: "lib/full.js",
+        output: [
             {
                 file: "docs/ws-es5.js",
                 format: "iife",
                 name: "ws",
-                sourcemap: true,
             },
         ],
         plugins: [
+            resolve(),
             libVersion,
-            terser(),
         ]
     },
 ]
